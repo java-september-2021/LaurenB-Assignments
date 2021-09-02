9d33cb47cdde72a944db9474f1255221e9b58f9e
Lauren Babb <laurenjbabb@gmail.com>
2021-09-02 15:16:19 -0500

    basic assignment

--- 01-java-fundamentals/05-Basics/BasicJava.java
+++ 01-java-fundamentals/05-Basics/BasicJava.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 public class BasicJava {
 
     // PRINT 1-255
@@ -50,4 +52,70 @@ public class BasicJava {
         }
         return nums;
     }
-}
+
+    // GET AVERAGE
+    public double GetAverage(int[] number) {
+        int sum = GetSum(number);
+        double avg = (double)sum/number.length;
+        System.out.println(String.format("Average is %f", avg));
+        return avg;
+    }
+
+    // GREATER THAN Y
+    public int GreaterThanY (int[] arr, int y) {
+        int count = 0;
+        for (int num: arr) {
+            if (num > y)
+                count++;
+        }
+        return count;
+    }
+
+    //SQUATE THE VALUE
+    public void Square(int[] num) {
+        for(int i = 0; i < num.length; i++) {
+            num[i]= num[i] * num[i];
+            System.out.println(num[i]);
+        }
+    }
+
+    // ELIMINATE NEGATIVE NUMBERS
+    public void EliminateNegative(int[] num) {
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] < 0)
+                num[i]=0;
+                System.out.println(num[i]);
+        }    
+    }
+
+    // FIND MIN
+    public int FindMin(int[] numbers) {
+        int minValue = numbers[0];
+        for (int i =0; i < numbers.length; i++) {
+            if (numbers[i] < minValue) {
+                minValue= numbers[i];
+            }
+        }
+        return minValue;
+    }
+    // MAX, MIN, AVERAGE
+    public void MaxMinAverage(int[] num){
+        int max = FindMax(num);
+        int min = FindMin(num);
+        double avg = GetAverage(num);
+        System.out.println(String.format("Max is %d, Min is %d, Avg is %f", max, min, avg));
+
+    }
+
+    // SHIFTING THE VALUES IN THE ARRAY
+    public void ShiftArray(int[] arrnum) {
+        for (int i = 0; i < arrnum.length; i++) {
+            if (i == arrnum.length-1) {
+                arrnum[i]=0;
+                break;
+            }
+            arrnum[i] = arrnum[i+1];
+        }
+        System.out.println(Arrays.toString(arrnum));
+    }
+} 
--- 01-java-fundamentals/05-Basics/BasicJavaTest.java
+++ 01-java-fundamentals/05-Basics/BasicJavaTest.java
@@ -1,13 +1,21 @@
 public class BasicJavaTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BasicJava basic = new BasicJava();
-        int[] nums = {1,2,5,6,10};
+        int[] nums = { -1, 2, 5, 6, 10 };
+        int y = 3;
+
         // basic.OneToTwoFifty();
         // basic.PrintOdds();
         // System.out.println(basic.GetSum(nums));
         // basic.LoopArray(nums);
         // System.out.println(basic.FindMax(nums));
-        System.out.println(basic.ArrOddNum());
-        }
+        // System.out.println(basic.ArrOddNum());
+        // System.out.println(basic.GetAverage(nums));
+        //System.out.println(basic.GreaterThanY(nums, y));
+        //basic.Square(nums);
+        //basic.EliminateNegative(nums);
+        //basic.MaxMinAverage(nums);
+        basic.ShiftArray(nums);
+
     }
-    
+}
